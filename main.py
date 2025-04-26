a17abf5ea510c20e8708aadd1c4c47e348b70fc8
Nelson Kang <Nelsonkang7@gmail.com>
2025-04-26 11:03:20 -0400

    token

---
+++ main.py
@@ -0,0 +1,16 @@
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
+
+#STEP 0: Load token
+
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+print(TOKEN)
+
+
+
+
+
